5b5a7fd2b8f43bef47f14fe2b1b801044b049b8d
Seth <Sethstaggs2@gmail.com>
2021-04-01 14:49:44 -0500

    fixing proxy URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://git.heroku.com/shrouded-basin-13457.git'
+  uri: 'https://shrouded-basin-13457.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
